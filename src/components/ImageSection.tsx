4830cf22b6682c82b53015bcefd2c1862e810c94
Gala <calero.vg@gmail.com>
2025-02-12 21:33:43 +0100

    some renaming

--- src/components/ImageSection.tsx
+++ src/components/ImageSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 
-export default function HeroSection() {
+export default function ImageSection() {
   return (
     <section className="min-h-screen relative overflow-hidden">
       {/* Background Image */}
@@ -21,7 +21,7 @@ export default function HeroSection() {
       <div className="relative z-10 p-8 sm:p-20 flex items-center justify-start min-h-screen">
         <div className="max-w-3xl">
           <h1 className="text-5xl sm:text-6xl font-bold mb-6 text-white">
-            Hello! I'm Gala,
+            Hello! I&apos;m Gala,
           </h1>
           <p className="text-2xl sm:text-3xl text-gray-200">
             web3 frontend developer and privacy advocate
